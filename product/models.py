d447fdfe68ca79ecdb5f25f8770d9142abeb77f3
sabrina <peiching913@gmail.com>
2025-05-04 11:55:13 +0800

    fix

--- product/models.py
+++ product/models.py
@@ -10,5 +10,5 @@ class Product(models.Model):
     description = models.TextField()
     calories = models.IntegerField()
     quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=0)
     customize = models.TextField()
