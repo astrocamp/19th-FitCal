a7cdc6230a16cb69e8e9501d9a3c07292337b34c
Sachico <s104103045@gmail.com>
2025-05-15 14:11:44 +0800

    feat:add member and store favorite relationship

--- members/models.py
+++ members/models.py
@@ -27,7 +27,6 @@ class Member(models.Model):
     google_id = models.CharField(max_length=64, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    store = models.OneToOneField(Store, on_delete=models.CASCADE, null=True)
     favorite = models.ManyToManyField(
         Store, through='Favorite', related_name='favorited_by'
     )
