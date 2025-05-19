616ee5f9cc860eaf4185ea4cbe0ed11a9b224e1b
KWY <nivekyang12014@gmail.com>
2025-05-19 18:33:32 +0800

    feat: implement order fsm functionality and improved UI components

--- src/scripts/input.js
+++ src/scripts/input.js
@@ -4,11 +4,16 @@ import htmx from 'htmx.org';
 import cartItem from './carts/cartItemEdit.js';
 import { initPickupTimePicker } from './flatpickr/main.js';
 import { searchFormComponent } from './searchForm.js';
+import { copyToClipboard } from './utils/clipboard.js';
 
 window.Alpine = Alpine;
 window.htmx = htmx;
 window.flatpickr = flatpickr;
 window.searchFormComponent = searchFormComponent;
+window.copyToClipboard = copyToClipboard;
+
+Alpine.data('clipboard', copyToClipboard);
+
 initPickupTimePicker('#id_pickup_time', 'healthy_style');
 initPickupTimePicker('#id_date_of_birth', 'cool_style', {
   enableTime: false,
