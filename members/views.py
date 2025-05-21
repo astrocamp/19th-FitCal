8b64d117b6b04482738f79833d21511a070ddcce
Jiahe Wang <1995andywang@gmail.com>
2025-05-21 20:41:59 +0800

    fix: adjust required setting for member field & adjust views

--- members/views.py
+++ members/views.py
@@ -7,8 +7,6 @@ from django.db import transaction
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
 
-from users.forms import UserForm
-
 from .forms import MemberForm
 from .models import Member, Store
 
@@ -16,9 +14,9 @@ from .models import Member, Store
 def member_required(view_func):
     @wraps(view_func)
     def _wrapped_view(req, *args, **kwargs):
-        if not hasattr(req.user, 'member'):
+        if not req.user.is_member:
             messages.error(req, '您不是會員，無法訪問此頁面')
-            return redirect('users:select_role')
+            return redirect('users:index')
         return view_func(req, *args, **kwargs)
 
     return login_required(_wrapped_view)
@@ -29,99 +27,53 @@ def new(req):
     return render(req, 'members/new.html', {'form': form})
 
 
+@member_required
 @transaction.atomic
 def create_member(request):
-    user_data = request.session.get('temp_user_data')
-    role = request.session.get('temp_user_role')
-
-    if not user_data or role != 'member':
-        messages.error(request, '註冊流程不完整，請重新開始')
-        return redirect('users:select_role')
-
     if request.method == 'POST':
         form = MemberForm(request.POST)
         if form.is_valid():
-            user_form = UserForm(user_data)
-            if user_form.is_valid():
-                user = user_form.save(commit=False)
-                user.is_active = False
-                user.save()
-
-                member = form.save(commit=False)
-                member.user = user
-                member.save()
-
-                user.is_active = True
-                user.backend = 'django.contrib.auth.backends.ModelBackend'
-                user.save()
-
-                login(request, user)
-
-                del request.session['temp_user_data']
-                del request.session['temp_user_role']
-
-                return redirect('members:show', member.id)
-            else:
-                messages.error(request, '帳號資料驗證失敗，請重新註冊')
-                return redirect('users:sign_up')
-    else:
-        form = MemberForm()
-
-    return render(request, 'members/new.html', {'form': form})
+            member = form.save(commit=False)
+            member.user = request.user
+            member.save()
+            return redirect('members:show', member.id)
+    form = MemberForm()
+    return render(request, 'members/new.html', {'form': form}, status=400)
 
 
 @login_required
+@member_required
 def index(request):
-    try:
-        member = request.user.member
-    except Member.DoesNotExist:
-        return redirect('members:new')
+    member = Member.objects.filter(user=request.user).first()
     return render(request, 'members/index.html', {'member': member})
 
 
+@login_required
 @member_required
 def show(request, id):
     member = get_object_or_404(Member, pk=id, user=request.user)
-
     if request.method == 'POST':
-        form = MemberForm(request.POST, instance=member)
+        form = MemberForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('members:show', member.id)
         return render(request, 'members/show.html', {'member': member, 'form': form})
-
-    else:
-        form = MemberForm(instance=member)
-
-        return render(request, 'members/show.html', {'member': member, 'form': form})
+    return render(request, 'members/show.html', {'member': member})
 
 
+@login_required
 @member_required
 def edit(request, id):
     member = get_object_or_404(Member, pk=id, user=request.user)
-
-    if request.method == 'POST':
-        form = MemberForm(request.POST, instance=member)
-        if form.is_valid():
-            form.save()
-            return redirect('members:show', member.id)
-        else:
-            return render(
-                request, 'members/edit.html', {'form': form, 'member': member}
-            )
-    else:
-        form = MemberForm(instance=member)
-        return render(request, 'members/edit.html', {'form': form, 'member': member})
+    form = MemberForm(instance=member)
+    return render(request, 'members/edit.html', {'form': form, 'member': member})
 
 
 @member_required
 def delete(request, id):
     member = get_object_or_404(Member, pk=id, user=request.user)
-    user = request.user
-
     member.delete()
-    return redirect('members:index')
-
+    return redirect('users:sign_up')
 
 @require_POST
 @member_required
