1316821f52c63737ad921a7f3277b8603940adff
sabrina <peiching913@gmail.com>
2025-05-21 10:55:13 +0800

    Fix search bar in template and remove unnecessary files

--- src/scripts/input.js
+++ src/scripts/input.js
@@ -2,9 +2,11 @@ import Alpine from 'alpinejs';
 import flatpickr from 'flatpickr';
 import 'htmx.org';
 import { initPickupTimePicker } from './flatpickr/main.js';
+import { searchFormComponent } from './searchForm.js';
 
 window.Alpine = Alpine;
 window.flatpickr = flatpickr;
+window.searchFormComponent = searchFormComponent;
 initPickupTimePicker('#id_pickup_time', 'healthy_style');
 initPickupTimePicker('#id_date_of_birth', 'cool_style', {
   enableTime: false,
