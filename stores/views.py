6dd157ee3de1c54d7efd60c2558485a37f5b173f
黃韋翰 <ab000641@gmail.com>
2025-05-23 15:30:24 +0800

    Add rating feature for members to rate stores

--- stores/views.py
+++ stores/views.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
+from django.db.models import Avg
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.template.loader import render_to_string
+from django.views.decorators.http import require_POST
 
 from common.decorator import store_required
 
-from .forms import StoreForm
-from .models import Store
+from .forms import RatingForm, StoreForm
+from .models import Rating, Store
 
 
 def new(req):
@@ -30,8 +34,30 @@ def create_store(request):
 @login_required
 def index(request):
     stores = Store.objects.all()
-    store = Store.objects.filter(user=request.user).first()
-    return render(request, 'stores/index.html', {'stores': stores, 'store': store})
+    user = request.user
+
+    # 只有當使用者是會員且有對應 member 才抓取評分資料
+    member = (
+        getattr(user, 'member', None)
+        if user.is_authenticated and user.is_member
+        else None
+    )
+
+    for store in stores:
+        store.avg_rating = (
+            Rating.objects.filter(store=store).aggregate(avg=Avg('score'))['avg'] or 0
+        )
+        if member:
+            store.member_rating = Rating.objects.filter(
+                store=store, member=member
+            ).first()
+
+    context = {
+        'stores': stores,
+        'member': member,
+        'form': RatingForm() if member else None,
+    }
+    return render(request, 'stores/index.html', context)
 
 
 def show(req, id):
@@ -66,3 +92,36 @@ def delete(req, id):
     store = get_object_or_404(Store, pk=id, user=req.user)
     store.delete()
     return redirect('users:sign_up')
+
+
+@require_POST
+@login_required
+def rate_store(request, store_id):
+    member = getattr(request.user, 'member', None)
+    if not member:
+        return HttpResponse('只有會員可以評分', status=403)
+
+    store = get_object_or_404(Store, id=store_id)
+    rating = Rating.objects.filter(store=store, member=member).first()
+    form = RatingForm(request.POST, instance=rating)
+
+    if form.is_valid():
+        new_rating = form.save(commit=False)
+        new_rating.member = member
+        new_rating.store = store
+        new_rating.save()
+
+        # 如果是 HTMX 請求：回傳一個更新後的按鈕區塊
+        if request.headers.get('Hx-Request') == 'true':
+            html = render_to_string(
+                'stores/_rating_button.html',
+                {
+                    'store': store,
+                    'member_rating': new_rating,
+                },
+                request=request,
+            )
+            return HttpResponse(html)
+
+    # 傳統表單提交：重新導向回 index
+    return redirect('stores:index')
