e9e6c18d604de061ed660975b157ecb594d3cd49
Jiahe Wang <1995andywang@gmail.com>
2025-05-24 11:14:34 +0800

    fix: change attr member from User to Member & add total price calculate function in model

--- carts/models.py
+++ carts/models.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 
 from django.db import models
+from django.db.models import UniqueConstraint
 
 # from members.models import Member
 # from stores.models import Store
@@ -11,25 +12,35 @@ from django.db import models
 class Cart(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     note = models.TextField(blank=True, null=True)
-    total_price = models.PositiveIntegerField()
+    total_price = models.PositiveIntegerField(default=0)
     member = models.ForeignKey(
-        # 此處先以user測試，待user-member關聯後改為member
-        'users.User',
+        'members.Member',
         on_delete=models.CASCADE,
         related_name='carts',
     )
     store = models.ForeignKey(
         'stores.Store', on_delete=models.CASCADE, related_name='carts'
     )
-
-    # 下方為正常設定，但目前user-member尚未有對應關聯產生，先以上方預設值測試
-    # member = models.ForeignKey(
-    #     'members.Member', on_delete=models.CASCADE, related_name='carts'
-    # )
     cart_product = models.ManyToManyField('products.Product', through='carts.CartItem')
 
     class Meta:
-        unique_together = ('member', 'store')
+        constraints = [UniqueConstraint(fields=['member', 'store'], name='unique_cart')]
+
+    # 計算總價
+    def calculate_total_price(self):
+        # 利用 select_related 預先抓 product 避免N+1
+        items = self.items.select_related('product').all()
+
+        total = 0
+        for item in items:
+            total += item.product.price * item.quantity
+
+        return total
+
+    # 更新總價
+    def update_total_price(self):
+        self.total_price = self.calculate_total_price()
+        self.save(update_fields=['total_price'])
 
 
 class CartItem(models.Model):
@@ -38,8 +49,10 @@ class CartItem(models.Model):
     product = models.ForeignKey(
         'products.Product', on_delete=models.CASCADE, related_name='cart_items'
     )
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)
     customize = models.TextField(null=True, blank=True)
 
     class Meta:
-        unique_together = ('cart', 'product')
+        constraints = [
+            UniqueConstraint(fields=['cart', 'product'], name='unique_cart_item')
+        ]
