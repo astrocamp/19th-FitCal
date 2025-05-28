f71e85b990a8d15fdc55e7eb808986e433c8555a
黃韋翰 <ab000641@gmail.com>
2025-05-28 17:45:30 +0800

    feat: add registration and login pages for merchants

--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('sign_in/', views.sign_in, name='sign_in'),
     path('create_session/', views.create_session, name='create_session'),
     path('delete_session/', views.delete_session, name='delete_session'),
+    path('sign_up/store/', views.sign_up_store, name='sign_up_store'),
+    path('create_user/store/', views.create_user_store, name='create_user_store'),
+    path('sign_in/store/', views.sign_in_store, name='sign_in_store'),
+    path(
+        'create_session/store/', views.create_session_store, name='create_session_store'
+    ),
 ]
