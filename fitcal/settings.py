4f4a40849a1d1da754f1787fc84cceb17744757c
KWY <nivekyang12014@gmail.com>
2025-06-11 15:47:10 +0800

    feat: enhance unauthenticated user experience and access flow

--- fitcal/settings.py
+++ fitcal/settings.py
@@ -233,7 +233,7 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = 'users:sign_in'
 
 # 所有登入後的轉址地址
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'stores:index'
 
 # Debug Toolbar
 INTERNAL_IPS = ['127.0.0.1', 'localhost']
