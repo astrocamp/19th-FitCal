166eb57d0e7fb8b0b9c726f1be2a15e028c36fef
sabrina <peiching913@gmail.com>
2025-06-12 01:31:31 +0800

    refactor: improve Alpine.js logic in search bar

--- src/scripts/input.js
+++ src/scripts/input.js
@@ -4,19 +4,21 @@ import flatpickr from 'flatpickr';
 import htmx from 'htmx.org';
 import cartItem from './carts/cartItemEdit.js';
 import { DatePicker } from './datePicker.js';
-import { searchFormComponent } from './searchForm.js';
 import { initCategorySort } from './stores/categorySort.js';
 import { initProductSort } from './stores/productSort.js';
 import { initCategorySidebarActive } from './stores/selectCategory.js';
 import { initTabs } from './stores/selectTab.js';
 
+import { copyToClipboard } from './utils/clipboard.js';
 Chart.register(...registerables);
 window.Chart = Chart;
 
 window.Alpine = Alpine;
 window.htmx = htmx;
 window.flatpickr = flatpickr;
-window.searchFormComponent = searchFormComponent;
+window.copyToClipboard = copyToClipboard;
+
+Alpine.data('clipboard', copyToClipboard);
 
 Alpine.data('dataPicker', DatePicker);
 
